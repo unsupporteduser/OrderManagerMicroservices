3995c3ea2ffdeb6ba2ac5bb19cfaad0f304564d0
unsupporteduser <pavlo.kovgan@globallogic.com>
2018-11-15 12:02:38 +0200

    Create OrderManagerMicroservicesApplication.java

---
+++ OrderManagerMicroservices/src/main/java/com/pk/microservice/om/OrderManagerMicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.pk.microservice.om;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagerMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagerMicroservicesApplication.class, args);
+	}
+}
